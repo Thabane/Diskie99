e01b3391603999eb153eed82c25c12d8f4336783
Siyabonga Maboya <siyabonga@delft.co.za>
2017-11-13 13:07:00 +0200

    Million dollar commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,33 +3,39 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
+import { MyTeamsPage, TeamsPage, TeamDetailPage, GamePage, TeamHomePage, StandingsPage} from '../pages/pages'
 import { TabsPage } from '../pages/tabs/tabs';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    TabsPage,
+    MyTeamsPage,
+    TeamDetailPage,
+    TeamsPage,
+    GamePage,
+    TeamHomePage,
+    StandingsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    MyTeamsPage,
+    TeamDetailPage,
+    TeamsPage,
+    TabsPage,
+    GamePage,
+    TeamHomePage,
+    StandingsPage
   ],
   providers: [
     StatusBar,
